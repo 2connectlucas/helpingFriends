8175a0ad50eacabd35716b5efa0c5bc5e9d60647
DevMaxFlip <87001864+2connectlucas@users.noreply.github.com>
2024-05-12 02:02:58 -0300

    Add files via upload

---
+++ exemploArrays/arraysMult.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract ExemplosArrays {
+    string[4] public Alunos;
+    string[] public DisciplinaNome;
+    string[][] private MatrizesDiscProf;
+
+    function definirDisciplina(string memory nomeD) external {
+        DisciplinaNome.push(nomeD);
+    }
+
+    function deletarElemento(uint256 numeroD) external {
+        delete DisciplinaNome[numeroD];
+    }
+
+    function definirAluno(uint8 num, string memory novoAluno) external {
+        Alunos[num] = novoAluno;
+    }
+
+    function definirProfeDisciplina(string memory disciplina, string memory professor) external {
+        MatrizesDiscProf.push([disciplina, professor]);
+    }
+
+    function RetornarProfIndiceDisci(
+        uint256 Indicedisciplina,
+        uint256 IndiceProf
+    )
+
+        external 
+        view 
+        returns (string memory)
+    {
+        return MatrizesDiscProf[Indicedisciplina][IndiceProf];
+    }
+}
